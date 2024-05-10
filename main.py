1fed4d5fde55c2b4313f39d90d72cca869423a16
Ajaya <ajaya.ramachandran@gmail.com>
2024-05-09 23:09:10 -0400

    added full GUI navigation and mapped all button locations

--- main.py
+++ main.py
@@ -29,8 +29,11 @@ jsm.keylog
 
 fps = 60
 clock = pygame.time.Clock()
+
 initialTime = time.time()
 oldNews = True
+rowOffsetX = 0
+chaseOffsetX = 0
 
 menuButtons = [
     "Games",
@@ -43,6 +46,8 @@ games = [
     "Turrican",
     "Dance Dance Revolution",
     "Tetris",
+    "Terraria",
+    "Mariokart",
     "Poly Bridge"
 ]
 
@@ -84,7 +89,7 @@ def drawTestBG():
 running = True # Runs the game loop
 while running:
     screen.fill((10,10,10))
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 100, 100))  # Example rectangle
+    #pygame.draw.rect(screen, (0, 0, 0), (0, 0, 100, 100))  # Example rectangle
 
     #drawTestBG()
     jsCoords = jst.giveCoords() # retrieves the input coordinates from the relevant module
@@ -99,28 +104,30 @@ while running:
 
     centerX = 200
     centerY = windowSize[1] / 2
+    chaseOffsetX += (rowOffsetX - chaseOffsetX) * 0.05
     for game in games: # loops through all of the games to draw the boxes
-        pygame.draw.rect(screen, (255, 255, 255), (centerX - 150, centerY - 150, 300, 300), 2, border_radius=10)
-        buttonPositions.append((game, (centerX, centerY)))
+        pygame.draw.rect(screen, (255, 255, 255), (centerX - 150 + chaseOffsetX, centerY - 150, 300, 300), 2, border_radius=10)
+        buttonPositions.append((game, (centerX + chaseOffsetX, centerY)))
         centerX += 320
-    
     centerX = 20
     centerY = windowSize[1] - 90
     for menuButton in menuButtons: # loops through all of the menu buttons to draw the boxes
         pygame.draw.rect(screen, (255, 255, 255), (centerX, centerY, (windowSize[0] - 40 - 10*(len(menuButtons) - 1)) / len(menuButtons), 70), 2, border_radius=10)
         buttonPositions.append((menuButton, (centerX + (windowSize[0] - 40 - 10*(len(menuButtons) - 1)) / len(menuButtons) / 2, centerY + 35)))
         centerX += (windowSize[0] - 40 - 10*(len(menuButtons) - 1)) / len(menuButtons) + 10
+    pygame.draw.rect(screen, (255, 255, 255), (20, 20, 40, 40), 2, border_radius=10)
+    buttonPositions.append((menuButton, (40, 40)))
 
     ### BUTTON MANAGEMENT ###
     for index, object in enumerate(buttonPositions):
         if index == selected:
-            pygame.draw.circle(screen, (255, 255, 0), object[1], 5) # center of graph point
+            pygame.draw.circle(screen, (255, 255, 0), (object[1][0], object[1][1]), 5) # center of graph point
 
             relativePolarCoords = []
             for index1, object1 in enumerate(buttonPositions):
                 if index1 != index: # ignores the selected button when checking
                     relativePolarCoords.append((dist(object[1], object1[1]), dir(object[1], object1[1]), index1)) # grabs the distance to and direction to all of the buttons from the hovered button
-                    pygame.draw.aaline(screen, (255, 255, 255), object[1], object1[1])
+                    #pygame.draw.aaline(screen, (255, 255, 255), object[1], object1[1])
                     #pygame.draw.aaline(screen, (255, 255, 0), object[1], (object[1][0] + 128 * cos(relativePolarCoords[-1][1]), object[1][1] + 128 * sin(relativePolarCoords[-1][1])))
                     
             relativePolarCoords.sort(key = lambda x:x[0])
@@ -135,7 +142,8 @@ while running:
                     acceptedRelations.append(relation)
             
             for relation in acceptedRelations:
-                pygame.draw.aaline(screen, (255, 255, 0), object[1], (object[1][0] + 128 * cos(relation[1]), object[1][1] + 128 * sin(relation[1])))
+                #pygame.draw.aaline(screen, (255, 255, 0), object[1], (object[1][0] + 128 * cos(relation[1]), object[1][1] + 128 * sin(relation[1])))
+                None
     
     # We have found the branches of all the closest buttons to the selected. Next we need to figure out which one the player is going to based on their js position
 
@@ -152,6 +160,11 @@ while running:
 
         if branchDistances[0][0] < 0.5:
             selected = branchDistances[0][1]
+        
+        if buttonPositions[selected][1][0] > windowSize[0] - 200 and selected < len(buttonPositions) - 4:
+            rowOffsetX -= 320
+        if buttonPositions[selected][1][0] < 0:
+            rowOffsetX += 320
 
     if jsm.keylog[-1][0] == "-1":
         oldNews = False
